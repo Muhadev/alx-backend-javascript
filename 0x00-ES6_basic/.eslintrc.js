7cfcf154b76bf30bd82b0e5eaee24f7ec7749037
Muhadev <muhammedfayemi@gmail.com>
2024-05-27 22:13:36 +0100

    task is added

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    extends: 'airbnb-base',
+    env: {
+      browser: true,
+      node: true,
+      jest: true,
+      es2020: true,
+    },
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2020,
+      },
+      rules: {
+        // You can add any specific rule overrides here
+      },
+  };
+  

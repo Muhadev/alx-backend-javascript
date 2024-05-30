16b04b43bbec132b90c8cab44bea42697f323b61
Muhadev <muhammedfayemi@gmail.com>
2024-05-30 23:29:16 +0100

    task is added

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,6 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      throw new Error();
-    })
-    .finally(() => {
-     console.log('Request completed');
-    });
-  }
-export default handleResponseFromAPI;
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
+}

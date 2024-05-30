a494ec8c8d152ecc15fc750b8fd3dd9a9553d332
Muhadev <muhammedfayemi@gmail.com>
2024-05-30 23:55:29 +0100

    task is fix

--- 0x01-ES6_promise/utils.js
+++ 0x01-ES6_promise/utils.js
@@ -1,22 +1,13 @@
-// Simulated uploadPhoto function that returns a promise
-function uploadPhoto() {
-    return new Promise((resolve) => {
-      // Simulating uploading a photo
-      setTimeout(() => {
-        resolve({ status: 200, body: 'photo-profile-1' });
-      }, 1000); // Simulating a delay of 1 second
+export function uploadPhoto() {
+    return Promise.resolve({
+      status: 200,
+      body: 'photo-profile-1',
     });
   }
   
-  // Simulated createUser function that returns a promise
-  function createUser() {
-    return new Promise((resolve) => {
-      // Simulating creating a user
-      setTimeout(() => {
-        resolve({ firstName: 'Guillaume', lastName: 'Salva' });
-      }, 1500); // Simulating a delay of 1.5 seconds
+  export function createUser() {
+    return Promise.resolve({
+      firstName: 'Guillaume',
+      lastName: 'Salva',
     });
   }
-  
-  export { uploadPhoto, createUser };
-  

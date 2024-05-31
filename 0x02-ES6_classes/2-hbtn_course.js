67d902be9b5a9703bda7320d5b0498d2ec6aed6a
Muhadev <muhammedfayemi@gmail.com>
2024-05-31 03:03:20 +0100

    task is added

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,47 +1,71 @@
-class HolbertonCourse {
+/**
+ * Represents a Holberton Course.
+ */
+export default class HolbertonCourse {
+  /**
+   * Creates a new @see {@link HolbertonCourse}.
+   *
+   * @param {String} name - The name of the course.
+   * @param {Number} length - How long the course is (in months).
+   * @param {String[]} students - The names of students in the course.
+   */
   constructor(name, length, students) {
-    this._validateString(name, 'Name must be a string');
-    this._validateNumber(length, 'Length must be a number');
-    this._validateArray(students, 'Students must be an array of strings');
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
-  _validateString(value, errorMessage) {
-    if (typeof value !== 'string') {
-      throw new TypeError(errorMessage);
-    }
-  }
-  _validateNumber(value, errorMessage) {
-    if (typeof value !== 'number') {
-      throw new TypeError(errorMessage);
-    }
-  }
-  _validateArray(value, errorMessage) {
-    if (!Array.isArray(value) || !value.every(item => typeof item === 'string')) {
-      throw new TypeError(errorMessage);
-    }
-  }
+
+  /**
+   * Gets the name of this course.
+   */
   get name() {
     return this._name;
   }
+
+  /**
+   * Sets the name of this course.
+   */
   set name(value) {
-    this._validateString(value, 'Name must be a string');
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
     this._name = value;
   }
+
+  /**
+   * Gets the length of this course (in months).
+   */
   get length() {
     return this._length;
   }
+
+  /**
+   * Sets the length of this course (in months).
+   */
   set length(value) {
-    this._validateNumber(value, 'Length must be a number');
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
     this._length = value;
   }
+
+  /**
+   * Gets the names of students in this course.
+   */
   get students() {
     return this._students;
   }
+
+  /**
+   * Sets the names of students in this course.
+   */
   set students(value) {
-    this._validateArray(value, 'Students must be an array of strings');
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._students = value;
   }
 }
-export default HolbertonCourse;
--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,41 +1,30 @@
-class Currency {
-  constructor(code, name) {
-    this._validateString(code, 'Code must be a string');
-    this._validateString(name, 'Name must be a string');
-    this._code = code;
-    this._name = name;
-  }
-  _validateString(value, errorMessage) {
-    if (typeof value !== 'string') {
-      throw new TypeError(errorMessage);
+export default class Currency {
+    constructor(code, name) {
+      this.code = code;
+      this.name = name;
     }
-  }
   
-    // Getter and Setter for code
     get code() {
       return this._code;
     }
   
     set code(value) {
-      this._validateString(value, 'Code must be a string');
       this._code = value;
     }
   
-    // Getter and Setter for name
     get name() {
       return this._name;
     }
   
     set name(value) {
-      this._validateString(value, 'Name must be a string');
       this._name = value;
     }
   
-    // Method to display full currency
+    /**
+     * Creates the full string representation of this Currency.
+     * @returns {String}
+     */
     displayFullCurrency() {
-      return `${this._name} (${this._code})`;
+      return `${this.name} (${this.code})`;
     }
   }
-  
-  export default Currency;
-  

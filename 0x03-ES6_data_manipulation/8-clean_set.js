48c2cbd7abfdc83e434cbc6a03dbd5443d1a0ef7
Muhadev <muhammedfayemi@gmail.com>
2024-06-04 19:00:13 +0100

    ttask is added

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,6 @@
-// 8-clean_set.js
-
 function cleanSet(set, startString) {
-    // Filter the set values that start with the specified startString
-    const filteredValues = [...set].filter(value => value.startsWith(startString));
-    
-    // Map over the filtered values and remove the startString from each value
-    const cleanedValues = filteredValues.map(value => value.slice(startString.length));
-    
-    // Join the cleaned values into a single string separated by '-'
-    return cleanedValues.join('-');
-  }
-  
-  export default cleanSet;
-  
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
+  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
+  return cleanedValues.join('-');
+}
+export default cleanSet;

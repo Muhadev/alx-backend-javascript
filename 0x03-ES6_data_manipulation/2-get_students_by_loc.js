a8edbc73fa821bee8b5120017480ca4abbaa5563
Muhadev <muhammedfayemi@gmail.com>
2024-06-04 18:13:47 +0100

    task is added

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,8 +1,4 @@
-// 2-get_students_by_loc.js
-
 function getStudentsByLocation(students, city) {
-    return students.filter(student => student.location === city);
-  }
-  
-  export default getStudentsByLocation;
-  
+  return students.filter((student) => student.location === city);
+}
+export default getStudentsByLocation;

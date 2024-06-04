069a7432f9f0455cc0430c0000c36f67f143db64
Muhadev <muhammedfayemi@gmail.com>
2024-06-04 18:44:25 +0100

    task is added

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+function createInt8TypedArray(length, position, value) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+  view.setInt8(position, value);
+  return view;
+}
+export default createInt8TypedArray;

b76ff7607c3ede29346494ae79ddd3b9e7ef0bdd
Muhadev <muhammedfayemi@gmail.com>
2024-06-04 22:43:19 +0100

    task is added

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,15 @@
-function cleanSet(set, startString) {
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
-  return cleanedValues.join('-');
+export default function cleanSet(set, startString) {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
-export default cleanSet;

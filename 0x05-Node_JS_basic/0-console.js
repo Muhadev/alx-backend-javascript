2b926bf7e9be78c4c2834f08e1875086529520fa
Muhadev <muhammedfayemi@gmail.com>
2024-07-23 23:41:07 +0100

    task is added

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ * Displays a message on the STDOUT.
+ * param {String} msg The message to display.
+ * author Muhammed Olamilekan <https://github.Muhadev>
+ */
 function displayMessage(message) {
   console.log(message);
 }

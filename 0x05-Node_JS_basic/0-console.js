9365c42c47d8c190bb59db92281b9d7a2f175713
Muhadev <muhammedfayemi@gmail.com>
2024-07-23 23:50:18 +0100

    task is added

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -4,7 +4,7 @@
  * author Muhammed Olamilekan <https://github.Muhadev>
  */
 const displayMessage = (msg) => {
-    console.log(msg);
-  };
-  
+  console.log(msg);
+};
+
 module.exports = displayMessage;
